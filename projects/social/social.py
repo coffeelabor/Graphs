a388cec8c981a4a99cbb2c6c17ae5af7e9b0d5a6
Reed James <reedtjames1234@gmail.com>
2019-12-13 12:37:54 -0600

    Commit before push

--- projects/social/social.py
+++ projects/social/social.py
@@ -3,6 +3,8 @@
 class User:
     def __init__(self, name):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 class SocialGraph:
     def __init__(self):
@@ -47,8 +49,23 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
-
+        for i in range(num_users):
+            self.add_user(f'User {i+1}')
         # Create friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+        #shuffle the list,
+        random.shuffle(possible_friendships)
+
+        print(possible_friendships)
+
+        # then grab the first N elements from the list.
+        # Number of times to call add_friendship = avg_friendships * num_users / 2
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
 
     def get_all_social_paths(self, user_id):
         """
